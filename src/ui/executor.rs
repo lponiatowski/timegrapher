03dfd32231f25d9aab17fea72c583195d3dfe024
lukaponiatowski <144330769+lukaponiatowski@users.noreply.github.com>
2024-10-03 17:06:59 +0200

    Adding denoised into blocking thread

--- src/ui/executor.rs
+++ src/ui/executor.rs
@@ -3,7 +3,7 @@ use crate::audio::track::AudioTrack;
 use crate::signal::speexdsp;
 use crate::signal::utils;
 use std::sync::Arc;
-use tokio::{task::{spawn_local, LocalSet}, sync::Mutex, task::JoinHandle};
+use tokio::{spawn, sync::Mutex, task::JoinHandle};
 
 pub struct ExecutorCTL {
     pub rawdata: Arc<Mutex<AudioTrack>>,
@@ -18,16 +18,10 @@ pub fn spawn_executor(aust: AudioStream, ctl: ExecutorCTL) -> Option<JoinHandle<
     println!("Sampling initiated");
     // calclulate framesize
     let sampling_rate = aust.samplerate();
-    let frame_size: f64 = ctl.duration / sampling_rate;
+    let frame_size: f64 = ctl.duration * sampling_rate;
     let frame_size: i64 = frame_size.round() as i64;
 
-    let local = LocalSet::new();
-
-    let handle = local.spawn_local(async move {
-        let speex = speexdsp::Denoiser::new(frame_size as i32, sampling_rate as i32)
-            .set_ctl(speexdsp::ControlSet::SetDenoise, 1)
-            .set_ctl(speexdsp::ControlSet::SetAgc, 1);
-    
+    let handle = spawn(async move {
         loop {
             let mut track = aust.get_track_by_framesize(frame_size).await;
     
@@ -43,21 +37,28 @@ pub fn spawn_executor(aust: AudioStream, ctl: ExecutorCTL) -> Option<JoinHandle<
             // track = utils::sliding_max(track, 300);
             // track = utils::cutt_off( track, ctl.cutoff);
             // track = utils::abs(track);
-            let mut frame: Vec<f32> = track.get_volume().iter().map(|&v| v as f32).collect();
-            speex.process(&mut frame);
-            track.update_volume(frame);
+            let frame: Vec<f32> = track.get_volume().iter().map(|&v| v as f32).collect();
+            let processed_frame = tokio::task::spawn_blocking(move || {
+                // Create the Denoiser and process the frame inside the blocking task
+                let speex = speexdsp::Denoiser::new(frame_size as i32, sampling_rate as i32)
+                    .set_ctl(speexdsp::SetControll::Denoise, 1)
+                    .set_ctl(speexdsp::SetControll::NoiseSuppress, 16000)
+                    .set_ctl(speexdsp::SetControll::Agc, 1)
+                    .set_ctl(speexdsp::SetControll::AgcLevel, 8000);
+                
+                let mut frame = frame; // mutable frame
+                speex.process(&mut frame);
+                frame
+            })
+            .await
+            .unwrap();
     
+            track.update_volume(processed_frame);
+
             let mut data = ctl.data.lock().await;
             *data = track;
         }
     });
-
-    // Run the LocalSet on the current thread
-    tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(local);
     
     Some(handle)
 }
