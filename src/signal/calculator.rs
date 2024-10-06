fad229d20d1fb095abfcef7031c62b6d9eb095e5
lponiatowski <luka.poniatowski@gmail.com>
2024-10-06 22:43:02 +0200

    Shapong calculator

--- src/signal/calculator.rs
+++ src/signal/calculator.rs
@@ -1,4 +1,5 @@
 use crate::audio::track::AudioTrack;
+use crate::signal::utils;
 
 pub struct BitCalculator{}
 
@@ -21,6 +22,28 @@ impl BitCalculator{
     }
     
     pub fn run_calculator(track: &AudioTrack) -> Self{
+        let frame_size: f64 = 0.01 * track.get_sample_rate();
+        let frame_size: usize = frame_size.round() as usize;
+
+        let (min, max) = BitCalculator::get_min_max(track);
+        let mean = BitCalculator::get_mean(track);
+
+        let track = utils::remove_mean(track);
+
+        let threshhold = mean + 0.6*(max - mean);
+
+        let mut track = track.get_track();
+
+         
+        
+        for (t, v) in track.iter(){
+            let mut current_max = 0.0;
+            let mut max_time = 0.0;
+            if *v >= threshhold {
+                current_max = *v
+            }
+        }
+
         Self {  }
     }
 }
